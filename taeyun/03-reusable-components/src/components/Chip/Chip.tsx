bba7944ca5ccc0916175f81b5fb2ad1d99cd22da
taeyun01 <lwe12f675@gmail.com>
2024-06-29 04:04:48 +0900

    faet: 재사용 가능한 컴포넌트(Chip)

--- taeyun/03-reusable-components/src/components/Chip/Chip.tsx
+++ taeyun/03-reusable-components/src/components/Chip/Chip.tsx
@@ -1,16 +1,16 @@
 import { VariantProps, cva } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "rounded-lg border-4 border-white text-white inline-flex px-4 py-2",
+  "rounded-lg border-4 border-white inline-flex px-4 py-2 hover:opacity-50 transition-opacity cursor-pointer",
   {
     variants: {
       intent: {
-        primary: ["bg-blue-300"],
-        danger: "bg-red-300",
-        secondary: "bg-gray-300",
-        warning: "bg-yellow-300",
-        info: "bg-cyan-300",
-        default: "bg-black-300",
+        primary: "bg-blue-300 text-blue-900",
+        danger: "bg-red-300 text-red-900",
+        secondary: "bg-gray-300 text-gray-900",
+        warning: "bg-yellow-300 text-yellow-900",
+        info: "bg-cyan-300 text-cyan-900",
+        default: "bg-black-300 text-white",
       },
     },
     defaultVariants: {
@@ -34,5 +34,3 @@ const Chip = ({ intent, text, ...props }: ChipProps) => {
 };
 
 export default Chip;
-
-// Next.js + SupaBase + TanStackQuery + Todo
